176e7ebf581985128adfba471505962e7ef5f3e8
liyao.ryan <liyao.ryan@bytedance.com>
2021-08-12 01:27:04 +0800

    unit test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,15 +7,18 @@ import {run, release, build} from '../src/func/run'
 import {tmpdir} from 'os'
 
 const GITHUB_PAT = process.env.INPUT_TOKEN ?? process.env.GITHUB_TOKEN
+const TEST_OWNER = 'MBoxPlus'
+const TEST_REPO = 'mbox-workspace-test'
+const TEST_PLUGIN_NAME = 'MBoxWorkspaceTest'
 
 test('build package', async () => {
   const tmp = checkTempExists()
   cp.execSync(
-    `git clone https://${GITHUB_PAT}@github.com/MBoxPlus/mbox-workspace.git`,
+    `git clone https://${GITHUB_PAT}@github.com/${TEST_OWNER}/${TEST_REPO}.git`,
     {cwd: tmp}
   )
-  expect(await build(path.join(tmp, 'mbox-workspace'), tmp)).toBe(
-    path.join(tmp, 'mbox_workspace', 'release')
+  expect(await build(path.join(tmp, TEST_REPO), tmp)).toBe(
+    path.join(tmp, TEST_REPO, 'release')
   )
 }, 3000000)
 
@@ -23,45 +26,54 @@ test('release package', async () => {
   const tmp = checkTempExists()
   try {
     cp.execSync(
-      'curl -LO https://github.com/MBoxPlus/mbox-workspace/releases/download/v1.2.0/MBoxWorkspace.tar.gz',
+      `curl -LO https://github.com/${TEST_OWNER}/${TEST_REPO}/releases/download/v1.2.0/${TEST_PLUGIN_NAME}.tar.gz`,
       {cwd: tmp}
     )
   } catch (error) {
     console.log(error)
   }
 
-  cp.execSync('tar -zxf MBoxWorkspace.tar.gz', {cwd: tmp})
-  cp.execSync('rm MBoxWorkspace.tar.gz', {cwd: tmp})
+  cp.execSync(`tar -zxf ${TEST_PLUGIN_NAME}.tar.gz`, {cwd: tmp})
+  cp.execSync(`rm ${TEST_PLUGIN_NAME}.tar.gz`, {cwd: tmp})
   await expect(
     release(
       process.env['INPUT_TOKEN'] ?? '',
-      'MBoxPlus/mbox-workspace',
-      path.join(tmp, 'MBoxWorkspace'),
+      `${TEST_OWNER}/${TEST_REPO}`,
+      path.join(tmp, TEST_PLUGIN_NAME),
       false
     )
-  ).rejects.toThrow('[MBoxWorkspace]: v1.2.0 has already exists.')
+  ).rejects.toThrow(`[${TEST_PLUGIN_NAME}]: v1.2.0 has already exists.`)
 }, 300000)
 
 test('throws token missing', async () => {
   await expect(
     run({
       token: '',
-      repositoryName: 'MBoxPlus/mbox-workspace',
-      workspace: '/Users/yaoli/Desktop/ByteDance/MBoxPlus/mbox-workspace',
+      repositoryName: `${TEST_OWNER}/${TEST_REPO}`,
+      workspace: `xxxx`,
       force: false
     })
   ).rejects.toThrow(`Input 'token' is missing.`)
 })
 
-// // shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   const np = process.execPath
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-//   const options: cp.ExecFileSyncOptions = {
-//     env: process.env
-//   }
-//   console.log(cp.execFileSync(np, [ip], options).toString())
-// })
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  const tmp = checkTempExists()
+  cp.execSync(
+    `git clone https://${GITHUB_PAT}@github.com/${TEST_OWNER}/${TEST_REPO}.git`,
+    {cwd: tmp}
+  )
+  process.env['INPUT_FORCE'] = 'false'
+  process.env['GITHUB_REPOSITORY'] = `${TEST_OWNER}/${TEST_REPO}`
+  process.env['GITHUB_WORKSPACE'] = path.join(tmp, TEST_REPO)
+
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+})
 
 function checkTempExists() {
   const tmp = path.join(tmpdir(), 'mbox_github_actions')
